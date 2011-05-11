5de992ae1588dbf659125dc777aad7304e11f61e
ymirpl <marcin.mincer@gmail.com>
2011-05-11 14:14:06 +0200

    tuple

---
+++ src/edu/pw/gis/graph/Tuple.java
@@ -0,0 +1,12 @@
+package edu.pw.gis.graph;
+
+public class Tuple {
+	public Node n;
+	public Edge e;
+	
+	public Tuple(Node n, Edge e) {
+		this.n = n; // target node
+		this.e = e; // using edge
+	}
+	
+}
